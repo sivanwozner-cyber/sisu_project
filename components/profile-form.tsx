31b70193fb37b7d8a4c42af2730111bf0307d970
sivanwozner-cyber <sivanwozner@gmail.com>
2026-06-08 15:23:27 +0300

    auto: sync changes [hook]

--- components/profile-form.tsx
+++ components/profile-form.tsx
@@ -6,6 +6,7 @@ import { useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
 
 type Club = { slug: string; name: string };
 
